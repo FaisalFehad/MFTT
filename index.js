28b9ac03b9c811fdb0ca6399526b1da15ecdb0c0
FaisalFehad <faisal.fehad@pm.me>
2020-11-17 08:49:45 +0000

    Solution

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 exports.top_3_words = (s) => {
-  let result = '';
+  const result = [];
   const splittedString = s.toLowerCase().split(' '); // lowercased array of the input
-
   const wordsCount = {}; // keeps track of repeated words
+  const numWord = {};
+
+  // keep key value pairs {word : mentions}
   splittedString.forEach((word) => {
     if (wordsCount[word]) wordsCount[word] = wordsCount[word] += 1;
     else wordsCount[word] = 1;
   });
 
-  const topWordRepeats = Object.values(wordsCount).sort().reverse().splice(0, 3); // top 3 char counts
+  // count of the top 3 repeated words
+  const topWordRepeats = Object.values(wordsCount).sort().reverse().splice(0, 3);
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const [word, count] of Object.entries(wordsCount)) {
-    if (topWordRepeats.includes(count)) result += `${word} `;
-  }
+  // stores the top 3 repeated char as {'repeats': [ [word1, word1], [word2, word2]]}
+  // solves a problem where different words are repeated the same number of times
+  // only iterate through the top 3 words
+  Object.entries(wordsCount).forEach(([key, value]) => {
+    if (topWordRepeats.includes(value)) {
+      const arr = Array(1).fill(key);
+      if (numWord[value]) numWord[value] = [numWord[value], arr];
+      else numWord[value] = arr;
+    }
+  });
 
-  return [result.slice(0, -1)]; // remove space at the end of the string
+  // pushes repeated words to results
+  topWordRepeats.forEach((num) => {
+    if (numWord[num].length === 1 && result.length < 3) result.push(numWord[num][0]);
+    else numWord[num][0].map((val) => result.length < 3 && result.push(val)[0]);
+  });
+
+  //   console.log(result, 'res');
+  return result;
 };
--- index.spec.js
+++ index.spec.js
@@ -23,4 +23,18 @@ describe('top_3_words', () => {
     expect(top_3_words('hi')).to.eql(['hi']);
     expect(top_3_words('hElLo')).to.eql(['hello']);
   });
+  it('returns returns array of repeated words', () => {
+    const input1 = 'e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e';
+    const input2 = 'In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.';
+    expect(top_3_words(input1)).to.eql(['e', 'ddd', 'aa']);
+    expect(top_3_words(input2)).to.eql(['a', 'of', 'on']);
+  });
+  it('returns returns array of repeated words when passed long input', () => {
+    const input = 'In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.';
+    expect(top_3_words(input)).to.eql(['a', 'of', 'on']);
+  });
+  it('returns returns array of repeated words: ignoring special chars', () => {
+    const input = " //wont won't won't";
+    expect(top_3_words(input)).to.eql(["won't", 'wont']);
+  });
 });
